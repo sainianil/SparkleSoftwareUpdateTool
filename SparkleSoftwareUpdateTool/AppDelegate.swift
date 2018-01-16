7fd3cd212187bca37d34ffabfc39b72304be408b
Tyler Hostager <tyhostager@gmail.com>
2018-01-15 21:05:53 -0600

    Update: Converted project to modern swift 4 syntax, fixed issues with NSUrlSession since it is no longer supported, fixed App Transport Security issues to allow arbitrary uploads allowing http requests--https only allowed by default, added DSA public key

--- SparkleSoftwareUpdateTool/AppDelegate.swift
+++ SparkleSoftwareUpdateTool/AppDelegate.swift
@@ -5,22 +5,21 @@
 //  Created by Anil Saini on 12/6/15.
 //  Copyright © 2015 Anil Saini. All rights reserved.
 //
-
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
